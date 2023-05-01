4b2b5545920128f9cecec1c971f4e9a5f9c77918
oebolaji <obafemi.bolaji@yahoo.com>
2023-05-01 03:41:43 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
 import { Component } from '@angular/core';
+import {
+  Firestore,
+  collection,
+  addDoc,
+  collectionData,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ang-fire';
+  userData: Observable<any> | undefined;
+
+  constructor(private firestore: Firestore) {
+    this.getData();
+  }
+
+  addData(f: any) {
+    // console.log(f.value);
+    const collectionInstance = collection(this.firestore, 'users');
+    addDoc(collectionInstance, f.value)
+      .then(() => {
+        console.log('Data Saved Successfully');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getData() {
+    const collectionInstance = collection(this.firestore, 'users');
+    //oonly subscribing here to log the data
+    collectionData(collectionInstance, { idField: 'id' }).subscribe((val) => {
+      console.log(val);
+    });
+
+    //added the object {idField: 'id'} to the collection data to get an id field from the collection.
+    // the 'id' is a name given to the idField
+    this.userData = collectionData(collectionInstance, { idField: 'id' });
+  }
+
+  updateData(id: string) {
+    const docInstance = doc(this.firestore, 'users', id);
+
+    //hardcoded to replace a real life form situation
+    const updateData = {
+      name: 'updatedName',
+    };
+    updateDoc(docInstance, updateData)
+      .then(() => {
+        console.log('Data Updated');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  deleteData(id: string) {
+    const docInstance = doc(this.firestore, 'users', id);
+    deleteDoc(docInstance)
+    .then(() => {
+      console.log('Data Deleted successfully');
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const  environment = {
+  firebase: {
+    projectId: 'ang-fire-b676d',
+    appId: '1:684057761446:web:3b9861d6d360feeb9b0667',
+    storageBucket: 'ang-fire-b676d.appspot.com',
+    apiKey: 'AIzaSyDioEQl48ZbX3UOqdyaU4EX3eFh78_DEeU',
+    authDomain: 'ang-fire-b676d.firebaseapp.com',
+    messagingSenderId: '684057761446',
+  },
+  production: false
+}
